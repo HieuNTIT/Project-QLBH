a7c241f1089ca5070002090a6237bb4e98adb78a
hieu <hieucon36@gmail.com>
2020-01-05 01:00:19 +0700

    qlbh
    # Conflicts:
    #	HoaDon.html
    #	NhapHang.js
    #	QLSP.html
    #	QLSP.js

--- QLSP.js
+++ QLSP.js
@@ -1,11 +1,13 @@
 function getDefaultShoes() {
   return {
-    'imgUrl' : '',
+    'imgUrl': '',
     'maSanPham': '',
     'tenSanPham': '',
     'tenThuongHieu': '',
     'size': 0,
     'soLuong': 0,
+    'giaBan':0,
+    'giaMua':0,
   };
 };
 let listShoes = [];
@@ -16,33 +18,42 @@ shoes1.tenSanPham = 'Adidas Ultraboost';
 shoes1.tenThuongHieu = 'Adidas';
 shoes1.size = 40;
 shoes1.soLuong = 10;
+shoes1.giaBan = 3.95e6;
+shoes1.giaMua = 3.9e6;
 listShoes.push(shoes1);
 
-let shoes2 = getDefaultShoes();
+let shoes2 = getDefaultShoes(); 
+shoes2.imgUrl = 'image/Alphaboost.png';
 shoes2.imgUrl="image/Alphaboost.png"
 shoes2.maSanPham = 'SP002';
 shoes2.tenSanPham = 'Adidas Alphaboost';
 shoes2.tenThuongHieu = 'Adidas';
 shoes2.size = 41;
 shoes2.soLuong = 15;
+shoes2.giaBan = 3.95e6;
+shoes2.giaMua = 3.9e6;
 listShoes.push(shoes2);
 
 let shoes3 = getDefaultShoes();
-shoes3.imgUrl="image/epic.jfif"
+shoes3.imgUrl = 'image/Ultraboost.jpg';
 shoes3.maSanPham = 'SP003';
 shoes3.tenSanPham = 'Nike Epic React';
 shoes3.tenThuongHieu = 'Nike';
 shoes3.size = 42;
 shoes3.soLuong = 10;
+shoes3.giaBan = 2.6e6;
+shoes3.giaMua = 2.5e6;
 listShoes.push(shoes3);
 
 let shoes4 = getDefaultShoes();
-shoes4.imgUrl = "image/nikeair.jpg"
+shoes4.imgUrl = 'image/Ultraboost.jpg';
 shoes4.maSanPham = 'SP004';
 shoes4.tenSanPham = 'Nike Air max';
 shoes4.tenThuongHieu = 'Nike';
 shoes4.size = 43;
 shoes4.soLuong = 20;
+shoes4.giaBan = 2.96e6;
+shoes4.giaMua = 2.9e6;
 listShoes.push(shoes4);
 //LocalStorage
 
@@ -66,6 +77,8 @@ function displayListShoes(list) {
           <td>${shoes.tenThuongHieu}</td>
           <td>${shoes.size}</td>
           <td>${shoes.soLuong} Đôi</td>
+          <td>${shoes.giaBan}</td>
+          <td>${shoes.giaMua}</td>
           <td>
             <button class="btn btn-success" onclick="EditShoes('${shoes.maSanPham}')">Sửa</button>
             <button class="btn btn-warning" onclick="deleteClother('${shoes.maSanPham}')">Xóa</button>
@@ -76,6 +89,7 @@ function displayListShoes(list) {
   body.innerHTML = s;
 }
 displayListShoes(listShoes);
+
 //===============Edit=================//
 function addShoes(e) {
   e.preventDefault();
@@ -85,6 +99,8 @@ function addShoes(e) {
   let tenThuongHieu = document.getElementById('txtTHieu').value;
   let size = Number(document.getElementById('txtSize').value);
   let soLuong = Number(document.getElementById('txtSL').value);
+  let giaBan = Number(document.getElementById('txtGiaBan').value);
+  let giaMua = Number(document.getElementById('txtGiaMua').value);
   if (size <= 30) {
     alert("Mời nhập size")
   }
@@ -98,6 +114,9 @@ function addShoes(e) {
     newShoes.tenThuongHieu = tenThuongHieu;
     newShoes.size = Number(size);
     newShoes.soLuong = Number(soLuong);
+    newShoes.giaBan = Number(giaBan);
+    newShoes.giaMua = Number(giaMua);
+
     listShoes.push(newShoes);
     displayListShoes(listShoes);
     document.getElementById('frmnewShoes').reset();
@@ -114,6 +133,8 @@ function saveShoes(e) {
   let tenThuongHieu = document.getElementById('txtEditTHieu').value;
   let size = document.getElementById('txtEditSize').value;
   let soLuong = document.getElementById('txtEditSL').value;
+  let giaBan = document.getElementById('txtEditGiaBan').value;
+  let giaMua = document.getElementById('txtEditGiaMua').value;
   if (size <= 30) {
     alert("Mời nhập size")
   }
@@ -127,6 +148,8 @@ function saveShoes(e) {
         shoess.tenThuongHieu = tenThuongHieu;
         shoess.size = size;
         shoess.soLuong = soLuong;
+        shoess.giaBan = giaBan;
+        shoess.giaMua = giaMua;
         break;
       }
     }
@@ -179,12 +202,14 @@ function EditShoes(maSanPham) {
   document.getElementById('txtEditTHieu').value = currentShoes.tenThuongHieu;
   document.getElementById('txtEditSize').value = currentShoes.size;
   document.getElementById('txtEditSL').value = currentShoes.soLuong;
+  document.getElementById('txtEditGiaBan').value = currentShoes.giaBan;
+  document.getElementById('txtEditGiaMua').value = currentShoes.giaMua;
 
   localStorage.setItem('Allshoes', JSON.stringify(listShoes));
 }
 
 
-//Tìm kiếm 
+//=============Tìm Kiếm====================//
 function Search(){
   let newList = [];
   document.getElementById("status").innerHTML = "";
